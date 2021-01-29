a467db8939d344abe4cdec7415f44b18d21c0123
simone <simone@MBP-di-simone.home>
2021-01-29 17:25:48 +0100

    implemented localstorage

---
+++ localstorage.ts
@@ -0,0 +1,44 @@
+
+/**
+ * @param localstorageKey name for store data
+ * @param cacheTime milliseconds of cache time
+ * @param convert function to convert stored item
+ */
+function LocalStorage<T2>(
+    localstorageKey: string,
+    cacheTime: number = 0,
+    convert: (value: T2) => T2 = (value) => value) {
+    
+    return (target: any, propertyKey: string) => {
+        const props: PropertyDescriptor = {};
+            props.set = (value: any) => {
+                localStorage.setItem(localstorageKey, JSON.stringify({
+                    value,
+                    cacheTime,
+                    storedAt: new Date().getTime() + cacheTime
+                }));
+            }
+            props.get = () => {
+                const store = localStorage.getItem(localstorageKey);
+                
+                if(store){
+
+                    const parsed = JSON.parse(store);
+                    const value = parsed.value;
+                    const cacheTime_ = parsed.cacheTime;
+                    if(cacheTime_ && parsed.storedAt < new Date().getTime()) {
+                        localStorage.removeItem(localstorageKey);
+                        return null;
+                    }
+
+                    return convert(value);
+                }
+                return null;
+            }
+
+            Object.defineProperty(target, propertyKey, props);
+
+        }
+    
+        
+}

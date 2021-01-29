518a3bf7e8f58dda47ba3b15075581a3477c9bfa
simone <simone@MBP-di-simone.home>
2021-01-29 17:50:12 +0100

    fixed problem imports

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LocalStorage = void 0;
+var localstorage_1 = require("./localstorage");
+Object.defineProperty(exports, "LocalStorage", { enumerable: true, get: function () { return localstorage_1.LocalStorage; } });
---
+++ localstorage.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LocalStorage = void 0;
+/**
+ * @param localstorageKey name for store data
+ * @param cacheTime milliseconds of cache time
+ * @param convert function to convert stored item
+ */
+function LocalStorage(localstorageKey, cacheTime, convert) {
+    if (cacheTime === void 0) { cacheTime = 0; }
+    if (convert === void 0) { convert = function (value) { return value; }; }
+    return function (target, propertyKey) {
+        var props = {};
+        props.set = function (value) {
+            if (value === null || value === undefined) {
+                localStorage.removeItem(localstorageKey);
+            }
+            localStorage.setItem(localstorageKey, JSON.stringify({
+                value: value,
+                cacheTime: cacheTime,
+                storedAt: new Date().getTime() + cacheTime
+            }));
+        };
+        props.get = function () {
+            var store = localStorage.getItem(localstorageKey);
+            if (store) {
+                var parsed = JSON.parse(store);
+                var value = parsed.value;
+                var cacheTime_ = parsed.cacheTime;
+                if (cacheTime_ && parsed.storedAt < new Date().getTime()) {
+                    localStorage.removeItem(localstorageKey);
+                    return null;
+                }
+                return convert(value);
+            }
+            return null;
+        };
+        Object.defineProperty(target, propertyKey, props);
+    };
+}
+exports.LocalStorage = LocalStorage;
